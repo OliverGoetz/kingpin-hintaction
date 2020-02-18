74f8b6ab2dad50fbc30d4652c775aab56ec8f71e
Goetz, Oliver <oliver.goetz@sap.com>
2020-02-18 13:42:38 +0100

    Solution with fixed argument order
    Extracted argument definition into init methods.
    Go was probably parallelizing the var block, and due to a race condition
    the order was swapped with a hintaction method using non-local stuff.
    Just a guess, though...

--- kingpin-hintaction.go
+++ kingpin-hintaction.go
@@ -8,50 +8,75 @@ import (
 )
 
 var (
-	app = kingpin.New("kingpin-hintaction", "demo how hintaction breaks arg order")
-
-	correctOrder     = app.Command("correctorder", "Command with correct arg order")
-	correctOrderArg1 = correctOrder.Arg("arg1", "Argument 1").Required().HintAction(giveHintStatic).String()
-	correctOrderArg2 = correctOrder.Arg("arg2", "Argument 2").Required().String()
-
-	brokenOrder     = app.Command("brokenorder", "Command with broken arg order")
-	brokenOrderArg1 = brokenOrder.Arg("barg1", "Argument 1").Required().HintAction(giveHintFromVar).String()
-	brokenOrderArg2 = brokenOrder.Arg("barg2", "Argument 2").Required().String()
-
+	app    = kingpin.New("kingpin-hintaction", "demo how hintaction breaks arg order")
 	argSet = app.Flag("argset", "set of arguments to use").Default("x").String()
 )
 
+var (
+	correctOrder     *kingpin.CmdClause
+	correctOrderArg1 *string
+	correctOrderArg2 *string
+)
+
+func initCorrectOrderCommand() {
+	correctOrder = app.Command("correctorder", "Command with correct arg order")
+	correctOrderArg1 = correctOrder.Arg("arg1", "Argument 1").Required().HintAction(giveHintStatic).String()
+	correctOrderArg2 = correctOrder.Arg("arg2", "Argument 2").Required().String()
+
+	correctOderCommand := &correctOrderCommandStruct{}
+	correctOrder.Action(correctOderCommand.run)
+}
+
+var (
+	fixedOrder     *kingpin.CmdClause
+	fixedOrderArg1 *string
+	fixedOrderArg2 *string
+)
+
+func initFixedOrderCommand() {
+	fixedOrder = app.Command("fixedorder", "Command with fixed arg order")
+	fixedOrderArg1 = fixedOrder.Arg("barg1", "Argument 1").Required().HintAction(giveHintFromVar).String()
+	fixedOrderArg2 = fixedOrder.Arg("barg2", "Argument 2").Required().String()
+
+	fixedOderCommand := &fixedOrderCommandStruct{}
+	fixedOrder.Action(fixedOderCommand.run)
+}
+
 type correctOrderCommandStruct struct{}
 
-type brokenOrderCommandStruct struct{}
+type fixedOrderCommandStruct struct{}
 
 func giveHintStatic() []string {
+	log.Printf("Executing giveHintStatic")
 	return []string{"foo", "bar"}
 }
 
 var args = []string{"foo", "bar"}
 
 func giveHintFromVar() []string {
+	log.Printf("Executing giveHintFromVar")
 	return args
 }
 func (n *correctOrderCommandStruct) run(c *kingpin.ParseContext) error {
 	log.Printf("correct order command")
+	log.Printf("arg1: %v, arg2: %v", *correctOrderArg1, *correctOrderArg2)
+	log.Printf("Hints: %v", giveHintStatic())
 	return nil
 }
 
-func (n *brokenOrderCommandStruct) run(c *kingpin.ParseContext) error {
-	log.Printf("broken order command")
+func (n *fixedOrderCommandStruct) run(c *kingpin.ParseContext) error {
+	log.Printf("fixed order command")
+	log.Printf("barg1: %v, barg2: %v", *fixedOrderArg1, *fixedOrderArg2)
+	log.Printf("Hints: %v", giveHintFromVar())
 	return nil
 }
 
 func main() {
+	initCorrectOrderCommand()
+	initFixedOrderCommand()
+
 	app.Version("0.1")
 	app.HelpFlag.Short('h')
-
-	correctOderCommand := &correctOrderCommandStruct{}
-	correctOrder.Action(correctOderCommand.run)
-	brokenOderCommand := &brokenOrderCommandStruct{}
-	brokenOrder.Action(brokenOderCommand.run)
 
 	_, err := app.Parse(os.Args[1:])
 	if err != nil {
